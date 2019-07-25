beaae3f160b70c27666f3aadbad4491f8baa6d1c
BePsvPT <BePsvPT@users.noreply.github.com>
2019-07-25 23:45:07 +0800

    cleanup n-repeated-element-in-size-2n-array

--- algorithms/easy/961-n-repeated-element-in-size-2n-array.js
+++ algorithms/easy/961-n-repeated-element-in-size-2n-array.js
@@ -3,14 +3,8 @@
  * @return {number}
  */
 var repeatedNTimes = function(A) {
-    let i = A.indexOf(0);
-
-    if (i > -1 && A.includes(0, i + 1)) {
-        return 0;
-    }
-
-    for (i = 0; i < A.length; ++i) {
-        if ((A[i] / A[i+1]) === 1 || (A[i] / A[i+2]) === 1) {
+    for (let i = 0; i < A.length; ++i) {
+        if ((A[i] === A[i+1]) || (A[i] === A[i+2])) {
             return A[i];
         }
     }
